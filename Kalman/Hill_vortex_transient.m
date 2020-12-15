92ba8c1492cdb1a30435fd8d0f8d6f847cec378e
amir-cardiolab <76059225+amir-cardiolab@users.noreply.github.com>
2020-12-15 11:50:36 -0700

    Add files via upload

---
+++ Kalman/Hill_vortex_transient.m
@@ -0,0 +1,27 @@
+function RHS = Hill_vortex_transient(t,x)
+% Falahatpisheh & Kheradvar 2015 paper
+%R=1
+
+T=100; 
+
+A = sin(2*pi*t/T);
+
+r = sqrt( x(1)^2 + (x(2)-A)^2 + (x(3)-A)^2);
+
+    
+if (r<= 1)
+ u = x(1)^2 + 1 - 2*r^2;
+ v = x(1)*x(2);
+ w = x(1)*x(3);
+else
+ u = (x(3)^2)*(r^-5) - 1/3* (r^-3) - 2/3;
+ v = x(1)*x(2)*(r^-5);
+ w = x(1)*x(3)*(r^-5);   
+    
+end
+
+RHS = [u; v; w];
+
+
+
+end
